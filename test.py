394dd4e15f26742ee0a26c66b837f74cd2d71693
THINGUYEN\vicky <tnguyen143@ivytech.edu>
2024-04-18 12:21:26 -0400

    Second test to check sum of infractions and add header and explain the results

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+# Author: Thi Nguyen
+# File name: test.py
+# Description:  Testing program to check if the sum function in the my_sum.py module behaves as expected under different scenarios
+
 import unittest
 
 from my_sum import sum
+from fractions import Fraction
 
 
 class TestSum(unittest.TestCase):
@@ -12,5 +17,19 @@ class TestSum(unittest.TestCase):
         result = sum(data)
         self.assertEqual(result, 6)
 
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+
 if __name__ == '__main__':
     unittest.main()
+
+
+# After running the test, you’ll see the following information:
+# Results of all the tests, one failed (F) and one passed (.)
+# Details of failed test (test_list_fraction)
